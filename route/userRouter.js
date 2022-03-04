59d51612d2ad2d017108b2566e0fa7518c84cc48
merciof <merciofilho@gmail.com>
2022-03-04 17:34:05 -0300

    add user read function route

--- route/userRouter.js
+++ route/userRouter.js
@@ -5,5 +5,6 @@ import { userController } from "../controll/userController.js";
 const userRouter = Router();
 
 userRouter.post("/usuarios", userController.create);
+userRouter.get("/usuarios/:id", userController.read);
 
 export default userRouter;
