8731236bd92e8c79e6e4cd4e6290f9ffad8122c0
merciof <merciofilho@gmail.com>
2022-03-04 17:18:32 -0300

    add userModel

---
+++ model/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    name: { type: String },
+    password: { type: String, required: true },
+    phones: [String],
+    role: { type: String, enum: ["Administrator", "User"], default: "User" },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("user", UserSchema);
+
+export default UserModel;
