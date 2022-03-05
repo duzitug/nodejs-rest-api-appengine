a89bf653a30f3d73badffde3d6de4c0b17d1fd59
merciof <merciofilho@gmail.com>
2022-03-05 11:13:06 -0300

    add routes to index and delete

--- route/userRouter.js
+++ route/userRouter.js
@@ -4,8 +4,10 @@ import { userController } from "../controll/userController.js";
 
 const userRouter = Router();
 
+userRouter.get("/usuarios", userController.index);
 userRouter.post("/usuarios", userController.create);
 userRouter.get("/usuarios/:id", userController.read);
 userRouter.put("/usuarios/:id", userController.update);
+userRouter.delete("/usuarios/:id", userController.delete);
 
 export default userRouter;
