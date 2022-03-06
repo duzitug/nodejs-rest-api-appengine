060b5eb51295e603e56ac02016e739c1fd0f6382
merciof <merciofilho@gmail.com>
2022-03-05 21:26:54 -0300

    add commentRouter

---
+++ route/commentRouter.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { CommentController } from "../controller/CommentController.js";
+import commentModel from "../model/commentModel.js";
+
+const commentController = new CommentController(commentModel);
+
+const commentRouter = Router();
+
+commentRouter.get(
+  "/comentario",
+  commentController.index.bind(commentController)
+);
+commentRouter.post(
+  "/comentario",
+  commentController.create.bind(commentController)
+);
+commentRouter.get(
+  "/comentario/:id",
+  commentController.read.bind(commentController)
+);
+// commentRouter.put("/comentario/:id", commentController.update);
+commentRouter.delete(
+  "/comentario/:id",
+  commentController.delete.bind(commentController)
+);
+
+export default commentRouter;
