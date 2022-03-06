996bdeb77ba4b83b2736022f24aa04dac24e8ba2
merciof <merciofilho@gmail.com>
2022-03-06 12:40:24 -0300

    add create and getCommentsByUserId methods

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -1,7 +1,27 @@
+import userModel from "../model/userModel.js";
 import { Controller } from "./Controller.js";
 
 export class CommentController extends Controller {
   constructor(model) {
     super(model);
+  }
+
+  async create(request, response) {
+    const comment = await this.model.create(request.body);
+    const user = await userModel.findById(request.params.userId);
+    
+    user.comments.push(comment);
+
+    await user.save();
+
+    response.send(comment);
+  }
+
+  async getCommentsByUserId(request, response) {
+    const user = await userModel
+      .findById(request.params.userId)
+      .populate("comments");
+
+    response.send(user.comments);
   }
 }
