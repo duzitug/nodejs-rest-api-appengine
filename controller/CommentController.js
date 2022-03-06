833e3c9533306ab9401ae62eaf12acee385f329e
merciof <merciofilho@gmail.com>
2022-03-05 21:53:08 -0300

    remove create method override

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -4,14 +4,4 @@ export class CommentController extends Controller {
   constructor(model) {
     super(model);
   }
-
-  async create(request, response) {
-    const { text } = request.body;
-
-    const comment = await this.model.create({
-      text,
-    });
-
-    response.send(comment);
-  }
 }
