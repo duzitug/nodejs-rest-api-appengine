1daddc54dbcbc7b7bdbdbb3b4c1d8216ae58c4e1
merciof <merciofilho@gmail.com>
2022-03-06 12:39:34 -0300

    add 'relation' to user

--- model/commentModel.js
+++ model/commentModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema(
   {
     text: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   },
   { timestamps: true }
 );
