299fd2539215e3f1ab7762efd2d3a6ee52313b94
merciof <merciofilho@gmail.com>
2022-03-05 21:25:00 -0300

    add commentRouter

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 import mongoose from "mongoose";
 import express from "express";
-
 import userRouter from "./route/userRouter.js";
+import commentRouter from "./route/commentRouter.js";
 
 mongoose
   .connect("mongodb+srv://merciof:32722212@cluster0.jsnbu.mongodb.net/base2")
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", userRouter);
+app.use("/api", commentRouter);
 
 app.listen(8080, () => {
   console.log(`App listening on port 8080`);
