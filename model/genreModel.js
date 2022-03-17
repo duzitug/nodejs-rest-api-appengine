5752ff45928e0b96533082b4622eff8c84ddf382
merciof <merciofilho@gmail.com>
2022-03-17 08:16:46 -0300

    ad genreModel "relationships"

--- model/genreModel.js
+++ model/genreModel.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const genreSchemma = new mongoose.Schema(
-  { name: { type: String }, id_antigo: { type: Number } },
+  {
+    name: { type: String },
+    books: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }],
+  },
   { timestamps: true }
 );
 
