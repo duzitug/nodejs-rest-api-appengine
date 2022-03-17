051f2478bc18d1cf464e50152b76308f4a9eae18
merciof <merciofilho@gmail.com>
2022-03-17 08:16:11 -0300

    add studentModel "relationships"

--- model/studentModel.js
+++ model/studentModel.js
@@ -5,6 +5,8 @@ const studentSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     nome: { type: String },
+    course: { type: mongoose.Schema.Types.ObjectId, ref: "course" },
+    books: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }],
   },
   { timestamps: true }
 );
